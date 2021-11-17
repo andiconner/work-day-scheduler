72a67ffb79f5d3dfc0f5ce1f45b4e4f54a1fca29
Andi Conner <andiconner@icloud.com>
2021-11-17 16:35:57 -0500

    final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,59 @@
-//initiate a moment object
+
+//initiate moment.js to display the current date and time when the page opens
 var NowMoment = moment()
 // display value of momento object in #displayMoment div
 var elemDisplayMoment = document.getElementById('displayMoment');
-elemDisplayMoment.innerHTML = NowMoment;
-//display  value of date object in #display
-var hour = document.querySelector('.hour')
-var timeBlock = document.querySelector('.time-block')
+var elemHourMoment = document.getElementById('hourMoment')
+elemDisplayMoment.innerHTML = NowMoment.format("MMM DD, YYYY");
+elemHourMoment.innerHTML = NowMoment.format('hh:mm a')
+
+
+var hour = moment().hour()
+var timeArr = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+var timeBlock = document.querySelectorAll('.time-block')
 var past = document.querySelector('.past')
-var present = document.querySelector('present')
-var future = document.querySelector('future')
+var present = document.querySelector('.present')
+var future = document.querySelector('.future')
+var removeTasks = document.getElementById('#remove-tasks')
 
 var tasks = {};
 
+
 //color-code each time-block
-function colorCode () {
-    if (hour === elemDisplayMoment) {
-        timeBlock[i].style.addClass('present');
-    }else (hour < elemDisplayMoment || hour > elemDisplayMoment);{
-        timeBlock [i].style.getElementsByClassName('past');
-        timeBlock[i].style.getElementsByClassName('future');
+function colorCode() {
+    for (let i = 0; i < timeBlock.length; i++) {
+        console.log(hour, timeArr[i])
+        if (hour > timeArr[i]) {
+            timeBlock[i].classList.add('past');
+        } else if (hour < timeArr[i]) {
+            timeBlock[i].classList.add('future');
+        } else {
+            timeBlock[i].classList.add('present');
+        }
+        if (localStorage.getItem(timeArr[i])) {
+            $('#' + timeArr[i]).val(localStorage.getItem(timeArr[i]))
+        }
     }
+
 }
 
-
 // create tasks inside the "time-block" textarea
-var createTask = function(taskText) {
-    var taskText = $("<textarea>").addClass('time-block');
-}  
+var createTask = function (taskText) {
+    var taskText = $('<textarea>').addClass('time-block');
+}
+
+$(document).ready(function () {
+    colorCode()
 
 
 // save task function and storage in localStorage
-$(".saveBtn").click(function() {
-    // get textarea values
-    var taskText = $(".time-block").val();
-    //save in tasks array
-    tasks.push({
-    text: taskText,
-    });
+$(".saveBtn").on("click", function (event) {
+        var id = $(this).attr('data-id')
+        var text = $('#' + id).val()
+        console.log(text)
+        localStorage.setItem(id, text);
 
-    saveTasks();
-},
-function loadTasks() {
-        tasks = JSON.parse(localStorage.getItem("tasks"));
-        // if nothing in localStorage, create a new object to track all task status arrays
-        if (!tasks) {
-            tasks = {
-                toDo: [],
-            };
-        }
-        // loop over object properties
-        $.each(tasks, function (list, arr) {
-            console.log(list, arr);
-            // then loop over sub-array
-            arr.forEach(function (task) {
-                createTask(task.text);
-            });
-        });
-},    
-function saveTasks() {
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-})
+    })
+});
+
+
